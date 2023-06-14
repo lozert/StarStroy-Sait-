e3debc4deadbbb5c0437740ff513394c1403fea0
lozert <90326151+lozert@users.noreply.github.com>
2023-06-14 21:21:32 +0500

    Add files via upload

---
+++ js/Scroll.js
@@ -0,0 +1,16 @@
+
+
+
+
+$('.nav a').click(function(){
+  $(".active").removeClass("active");     
+  $(this).addClass("active");
+  
+  $('html, body').stop().animate({
+    scrollTop: $($(this).attr('href')).offset().top - 160
+  }, 300);
+  return false;
+});
+// }
+// scrollNav();
+
---
+++ js/replace.js
@@ -0,0 +1,12 @@
+// document.querySelector('button').onclick = myClick;
+
+// function myClick(){
+//     //get input
+//     let input1 =document.querySelector('.zakaz-input1');
+//     let input2 =document.querySelector('.zakaz-input2');
+//     let input3 =document.querySelector('.zakaz-input3');
+//     console.log(input1.value);
+//     input1.value = '';
+//     input2.value = '';
+//     input3.value = '';
+//   }
